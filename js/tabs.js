b4eaf36a7913003d384357b8cb878a55ef8a1320
madcatjoey <bbiondi59@gmail.com>
2019-04-16 11:58:22 -0700

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,20 @@
+//javascript for LBCC faculty page tabs
+
+function openTab(TabName,elmnt,color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(TabName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
